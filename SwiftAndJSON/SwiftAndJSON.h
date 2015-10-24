32db5beb2985159dfcfd16d35e87a8ec583cd6dc
Cristián Arenas Ulloa <cristian.arenas@gmail.com>
2015-10-24 17:06:54 -0300

    Initial Commit

---
+++ SwiftAndJSON/SwiftAndJSON.h
@@ -0,0 +1,19 @@
+//
+//  SwiftAndJSON.h
+//  SwiftAndJSON
+//
+//  Created by Cristian Arenas Ulloa on 10/24/15.
+//  Copyright © 2015 Cristián Arenas Ulloa. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftAndJSON.
+FOUNDATION_EXPORT double SwiftAndJSONVersionNumber;
+
+//! Project version string for SwiftAndJSON.
+FOUNDATION_EXPORT const unsigned char SwiftAndJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAndJSON/PublicHeader.h>
+
+
